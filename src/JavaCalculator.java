a443e7c014e73b26ca87a312eebaba8f9ae5f019
Lizie <alisson.munoz@epn.edu.ec>
2024-06-27 23:02:48 -0500

    Botones Se añadio la funcionalidad a los botones del 0 al 9

--- src/JavaCalculator.java
+++ src/JavaCalculator.java
@@ -1,20 +1,24 @@
+import org.w3c.dom.Text;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class JavaCalculator {
 
     private JPanel JavaCalculator;
     private JTextField textField1;
-    private JButton button1;
+    private JButton raizButton;
     private JButton btnNine;
     private JButton btnFive;
     private JButton btnOne;
-    private JButton button5;
+    private JButton senButton;
     private JButton btnFour;
     private JButton btnSeven;
     private JButton btnPoint;
-    private JButton button9;
-    private JButton button10;
-    private JButton button11;
+    private JButton cosButton;
+    private JButton tanButton;
+    private JButton btnExp;
     private JButton btnEight;
     private JButton btnZero;
     private JButton btnSix;
@@ -27,15 +31,35 @@ public class JavaCalculator {
     private JButton btnEquals;
     private JButton btnClear;
 
+    public JavaCalculator() {
+
+        addNumberButtonListener(btnZero);
+        addNumberButtonListener(btnOne);
+        addNumberButtonListener(btnTwo);
+        addNumberButtonListener(btnThree);
+        addNumberButtonListener(btnFour);
+        addNumberButtonListener(btnFive);
+        addNumberButtonListener(btnSix);
+        addNumberButtonListener(btnSeven);
+        addNumberButtonListener(btnEight);
+        addNumberButtonListener(btnNine);
+    }
+
+    private void addNumberButtonListener(JButton button) {
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String buttonText = textField1.getText() + button.getText();
+                textField1.setText(buttonText);
+            }
+        });
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("JavaCalculator");
         frame.setContentPane(new JavaCalculator().JavaCalculator);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-    }
-
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
     }
 }
