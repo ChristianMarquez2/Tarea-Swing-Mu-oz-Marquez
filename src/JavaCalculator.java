d35aa2a36831f83c76f938b85c1196e6dba28b48
Lizie <alisson.munoz@epn.edu.ec>
2024-06-27 20:31:07 -0500

    JavaCalculator GUI Form

---
+++ src/JavaCalculator.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class JavaCalculator {
+
+    private JPanel JavaCalculator;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JavaCalculator");
+        frame.setContentPane(new JavaCalculator().JavaCalculator);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
